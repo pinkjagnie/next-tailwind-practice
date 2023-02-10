ee18c132189ce04ab7077937bd56de5a2fe36a7d
pinkjagnie <olga.krzysztofik@gmail.com>
2023-02-10 18:17:53 +0100

    Added work page

---
+++ src/pages/work.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Hero from "@/components/Hero";
+
+const WorkPage = () => {
+  return(
+    <section>
+      <Hero heading='My Work' message='This is some of my recent work traveling the world.'/>
+    </section>
+  )
+};
+
+export default WorkPage;
