f576706e5f104e65d3f130125f398261d3f494f5
pinkjagnie <olga.krzysztofik@gmail.com>
2023-02-11 09:48:30 +0100

    Fixed grid in Portfolio component on desktop

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,6 +3,9 @@ import Image from "next/image";
 
 import { portfolioData } from "@/constans/index";
 
+const firstPotroflio = portfolioData[0];
+const portoflio = portfolioData.slice(1, 5)
+
 const Portfolio = () => {
   return(
     <section className='max-w-[1240px] mx-auto pt-2 pb-16 text-center'>
@@ -10,15 +13,15 @@ const Portfolio = () => {
 
       <div className='grid grid-rows-none md:grid-cols-5 p-4 gap-4'>
         <div className='w-full h-full col-span-2 md:col-span-3 row-span-2'>
-          {portfolioData.map((image, index) => {
-            return(
-              <div key={index}>
-                <Image src={image.image} alt="/" width={677} height={451} />
-              </div>
-            )
-          })}
+          <Image src={firstPotroflio.image} alt="/" width={677} height={451} />
         </div>
-
+        {portoflio.map((image, index) => {
+          return(
+            <div key={index} className='w-full h-full relative'>
+              <Image src={image.image} alt="/" fill style={{objectFit: 'cover'}}/>
+            </div>
+          )
+        })}    
       </div>
     </section>
   )
