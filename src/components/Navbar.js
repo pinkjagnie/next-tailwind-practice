66855b7795acef80b007a670784802918119e210
pinkjagnie <olga.krzysztofik@gmail.com>
2023-02-11 12:18:00 +0100

    Fixed Navbar on mobiles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -48,10 +48,10 @@ const Navbar = () => {
         <div className={openNav ? 'sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'
               : 'sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'}>
           <ul>
-            <li className='p-4 text-4xl hover:text-gray-500'><Link href='/'>Home</Link></li>
-            <li className='p-4 text-4xl hover:text-gray-500'><Link href='/#gallery'>Gallery</Link></li>
-            <li className='p-4 text-4xl hover:text-gray-500'><Link href='/work'>Work</Link></li>
-            <li className='p-4 text-4xl hover:text-gray-500'><Link href='/contact'>Contact</Link></li>
+            <li className='p-4 text-4xl hover:text-gray-500' onClick={openNavHandler}><Link href='/'>Home</Link></li>
+            <li className='p-4 text-4xl hover:text-gray-500' onClick={openNavHandler}><Link href='/#gallery'>Gallery</Link></li>
+            <li className='p-4 text-4xl hover:text-gray-500' onClick={openNavHandler}><Link href='/work'>Work</Link></li>
+            <li className='p-4 text-4xl hover:text-gray-500' onClick={openNavHandler}><Link href='/contact'>Contact</Link></li>
           </ul>
         </div>
 
